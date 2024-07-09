a54c688cfa1c63725bfa79aad723ecad50ead907
Anoop Chaudhary <anooprahulchaudhary@gmail.com>
2024-07-09 18:49:27 +0530

    removed the id empty-id check, added todo to validate non empty fields

--- src/middleware/validateUpdateJob.js
+++ src/middleware/validateUpdateJob.js
@@ -1,12 +1,6 @@
-import URLParamaeterMissing from "../error/urlParameterMissing.js";
-
 function validateUpdateJob(req, res, next) {
   try {
-    const jobId = req.params.id;
-    if (!jobId) {
-      throw new URLParamaeterMissing(`Job id can't empty.`);
-    }
-
+    //todo - validate the fields which will be updated.
     next();
   } catch (error) {
     next(error);
